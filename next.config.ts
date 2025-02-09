d67dcfc0e139e0171a04dc095dfe3d445c416f1f
LorenzoElenkov <thevalorbg@gmail.com>
2025-02-09 20:56:32 +0200

    222

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // Enables static export
+  basePath: "/porcelanosa-bg", // Change this to match your repository name
   images: {
+    unoptimized: true, // Required for static hosting
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'img.icons8.com',
+        protocol: "https",
+        hostname: "img.icons8.com",
       },
     ],
   },
 };
-
 
 export default nextConfig;
