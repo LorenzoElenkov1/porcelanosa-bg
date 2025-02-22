148e0c49dde902443eb9cae95c9cf377b8e7ef7b
LorenzoElenkov <thevalorbg@gmail.com>
2025-02-22 13:09:26 +0200

    adsadsa

--- src/app/catalogues/_components/AboveTheFold/AboveTheFold.tsx
+++ src/app/catalogues/_components/AboveTheFold/AboveTheFold.tsx
@@ -12,13 +12,11 @@ type Props = {
 const AboveTheFold = ({ text, image }: Props) => {
   return (
     <div className="w-full min-h-[calc(100svh-400px)] relative mb-10">
-      <Image
-        fill
-        src={image.url}
-        alt={image.alt}
-        className="object-cover"
-      />
-      <h1 className="absolute top-[55%] translate-y-[-50%] left-[50%] translate-x-[-50%] flex flex-col items-center leading-none text-white text-[60px]">
+      <Image fill src={image.url} alt={image.alt} className="object-cover" />
+      <h1
+        style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}
+        className="absolute top-[55%] translate-y-[-50%] left-[50%] translate-x-[-50%] flex flex-col items-center leading-none text-white text-[60px]"
+      >
         {text}
       </h1>
     </div>
