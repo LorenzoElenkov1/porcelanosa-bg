6f8e908a91f87229bb2f7872c37188e7edb62288
LorenzoElenkov <thevalorbg@gmail.com>
2025-02-22 12:25:10 +0200

    asd

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Enables static export
-  basePath: "/porcelanosa-bg", // Change this to match your repository name
-  assetPrefix: "/",
   images: {
     unoptimized: true, // Required for static hosting
     remotePatterns: [
