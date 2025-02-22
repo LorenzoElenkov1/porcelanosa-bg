d63743d7db63c95bca89a7f5f87b316614c0f1af
LorenzoElenkov <thevalorbg@gmail.com>
2025-02-22 12:34:07 +0200

    fdas

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Enables static export
+  trailingSlash: true,
   images: {
-    unoptimized: true, // Required for static hosting
+    unoptimized: true, // Required for static hosting,
     remotePatterns: [
       {
         protocol: "https",
